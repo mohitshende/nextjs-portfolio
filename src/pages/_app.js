ed44098b01050212539105fdd5faa367f18e1107
Mohit Shende <mohitshende023@gmail.com>
2021-11-23 14:36:18 +0530

    Minor changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
-import Theme from '../styles/theme';
+import Theme from "../styles/theme";
+import Head from "next/Head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+        <Head>
+          <title>Portfolio</title>
+        </Head>
         <Component {...pageProps} />
       </Theme>
     </>
   );
 }
- 
